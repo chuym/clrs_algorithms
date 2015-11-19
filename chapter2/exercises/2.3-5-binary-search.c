e231fe332b369758fec86440ea0d9cb9b31109ab
Chuy Del Castillo <chuy@imchuy.com>
2015-11-19 15:19:47 -0700

    2.3-5 - Binary Search

---
+++ chapter2/exercises/2.3-5-binary-search.c
@@ -0,0 +1,78 @@
+/**
+ * 2.3-5 - Binary search
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int * search(int needle, int *list, size_t size)
+{
+  int *result = NULL;
+  int *remainder;
+  size_t middle, i, j;
+
+  if(size == 0) {
+    return result;
+  }
+
+  if(size == 1) {
+    if(needle == list[0]) {
+      result = &list[0];
+    }
+    return result;
+  }
+
+  middle = size / 2;
+
+  if(needle == list[middle]) {
+    result = &list[middle];
+  } else if(needle > list[middle]) {
+    remainder = malloc(sizeof(int) * (size - middle - 1));
+    i = 0;
+    for(j = middle + 1; j < size; j++) {
+      remainder[i] = list[j];
+      i++;
+    }
+    result = search(needle, remainder, size - middle - 1);
+    free(remainder);
+  } else {
+    remainder = malloc(sizeof(int) * middle);
+    for(j = 0; j < middle; j++) {
+      remainder[j] = list[j];
+    }
+
+    result = search(needle, remainder, middle);
+    free(remainder);
+  }
+
+  return result;
+}
+
+int main()
+{
+  int *result;
+  int testcase_a[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  int testcase_b[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+  result = search(5, NULL, 0);
+  printf("Should not find for empty list: %d\n", result == NULL);
+
+  result = search(2, testcase_a, 9);
+  printf("Should find 2 in testcase_a: %d\n", result != NULL);
+
+  result = search(9, testcase_a, 9);
+  printf("Should find 9 in testcase_a: %d\n", result != NULL);
+
+  result = search(11, testcase_a, 9);
+  printf("Should not find 11 in testcase_a: %d\n", result == NULL);
+
+  result = search(1, testcase_b, 10);
+  printf("Should find 1 in testcase_b: %d\n", result != NULL);
+
+  result = search(7, testcase_b, 10);
+  printf("Should find 7 in testcase_b: %d\n", result != NULL);
+
+  result = search(11, testcase_b, 10);
+  printf("Should not find 11 in testcase_b: %d\n", result == NULL);
+
+  return 0;
+}

0ecc44336fffb23306ffd83324f14064edc886f2
Chuy Martinez <chuy@imchuy.com>
2016-02-17 10:09:35 -0700

    Creating max heap structure separately

--- chapter6/heapify.py
+++ chapter6/heapify.py
@@ -12,38 +12,6 @@ class Heap:
         self[a] = self[b]
         self[b] = temp
 
-    def maximum(self):
-        return self[1]
-
-    def max_heapify(self):
-        for i in xrange(self.heap_size >> 1, 0, -1):
-            max_heapify(self,i)
-
-    def extract_max(self):
-        if self.heap_size < 1:
-            raise 'Heap underflow'
-
-        top = self[1]
-        self[1] = self[self.heap_size]
-        self.heap_size = self.heap_size - 1
-        self.max_heapify()
-        return top
-
-    def increase_key(self, i, key):
-        if key < self[i]:
-            raise "new key is smaller than current key"
-        self[i] = key
-        while i > 1 and self[parent(i)] < self[i]:
-            self.swap(i, parent(i))
-            i = parent(i)
-
-    def insert(self, key):
-        self.heap_size += 1
-        self.length += 1
-        self.elements.append(key)
-        print self.elements
-        self.increase_key(self.heap_size, key)
-
     def __getitem__(self, key):
         return self.elements[key-1]
 
@@ -55,54 +23,3 @@ class Heap:
 
     def __str__(self):
         return str(self.elements)
-
-def parent(i):
-    return i >> 1
-
-def left(i):
-    return i << 1
-
-def right(i):
-    return (i << 1) + 1
-    
-def max_heapify(A, i):
-    l = left(i)
-    r = right(i)
-
-    if l <= A.heap_size and A[l] > A[i]:
-        largest = l
-    else:
-        largest = i
-    if r <= A.heap_size and A[r] > A[largest]:
-        largest = r
-
-    if largest != i:
-        A.swap(i, largest)
-        return max_heapify(A, largest)
-
-    return A
-
-def min_heapify(A, i):
-    l = left(i)
-    r = right(i)
-
-    if l <= A.heap_size and A[l] < A[i]:
-        smallest = l
-    else:
-        smallest = i
-    if r <= A.heap_size and A[r] < A[smallest]:
-        smallest = r
-
-    if smallest != i:
-        temp = A[i]
-        A[i] = A[smallest]
-        A[smallest] = temp
-        return min_heapify(A, smallest)
-
-    return A
-
-# min_heapify, compared to max_heapify - the run both at time O(lg n)
-
-# Calling max_heapify when the element A[i] is larger than its children
-# will make the procedure stop and return at that point.
-# Since this is the the return point of the recursive procedure.
---
+++ chapter6/max_heap.py
@@ -0,0 +1,77 @@
+import heapify
+
+class MaxHeap(Heap):
+    def __init__(self, elements):
+        super(MaxHeap, self).__init__(elements)
+        self.max_heapify()
+    
+    def max_heapify(self):
+        for i in xrange(self.heap_size >> 1, 0, -1):
+            max_heapify(self, i)
+
+    def extract_max(self):
+        if self.heap_size < 1:
+            raise "Heap underflow"
+
+        top = self[1]
+        self[1] = self[self.heap_size]
+        self.heap_size -= 1
+        self.max_heapify()
+        return top
+
+    def maximum(self):
+        return self[1]
+
+    # Invariant:
+    # At the start of the while loop, self[parent(i)] >= self[left(i)] and
+    # self[parent(i)] >= self[right(i)] and self[1..heap_size] is a max heap
+    # but there may or may not be a violation with the key self[i] that was just
+    # inserted
+    #
+    # In the while loop, we check if i is at the root, if it is, there is nothing
+    # more to do and return. We also check if self[i] violates the max heap property
+    # by comparing it to its parent.
+    # If there is no violation, there's nothing more to do and self[1..heap_size] is
+    # a max heap. If there is a violation, we swap keys with its parent and iterate
+    # the while loop until we reach the root or there are no more max heap property
+    # violations.
+    def increase_key(self, i, key):
+        if key < self[i]:
+            raise "new key is smaller than current key"
+        self[i] = key
+        while i > 1 and self[parent(i)] < self[i]:
+            self.swap(i, parent(i))
+            i = parent(i)
+
+    def insert(self, key):
+        self.heap_size += 1
+        self.length += 1
+        self.elements.append(key)
+        print self.elements
+        self.increase_key(self.heap_size, key)
+
+def parent(i):
+    return i >> 1
+
+def left(i):
+    return i << 1
+
+def right(i):
+    return (i << 1) + 1
+    
+def max_heapify(A, i):
+    l = left(i)
+    r = right(i)
+
+    if l <= A.heap_size and A[l] > A[i]:
+        largest = l
+    else:
+        largest = i
+    if r <= A.heap_size and A[r] > A[largest]:
+        largest = r
+
+    if largest != i:
+        A.swap(i, largest)
+        return max_heapify(A, largest)
+
+    return A
